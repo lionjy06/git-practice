3ff32453566f63eeff24f036bfe4889934b2b71d
lionjy06 <jyjjyj06@naver.com>
2021-11-27 19:27:23 +0900

    review of html css

---
+++ reactive.js
@@ -0,0 +1,6 @@
+const toggleBtn =  document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu')
+
+toggleBtn.addEventListener('click',()=>{
+  navMenu.classList.toggle('active')
+})
